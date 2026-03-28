1558ff64708a8baa312ca3df27b20aec821a7ead
zamiltec <mahmoud@zamiltec.com>
2026-03-28 18:59:48 +0200

    format price

--- mswaffer/mswaffer/telegrambot.py
+++ mswaffer/mswaffer/telegrambot.py
@@ -32,7 +32,9 @@ async def send_direct_message(messageid):
         product_name= str(product_doc.product_name )
         product_price= str(product_doc.original_price )
         current_price= str(product_doc.current_price )
-        discount_percentage= str(product_doc.discount_percentage)
+        formatted_price_after = "{:,.0f}".format(current_price)
+        formatted_price_before = "{:,.0f}".format(product_price)
+        discount_percentage= str(int(round(int(product_doc.discount_percentage))))
         # 1. معالجة اسم المنتج (أول 30 حرف فقط) مع إضافة نقاط إذا كان أطول
         short_name = (product_name[:27] + '...') if len(product_name) > 30 else product_name
 
@@ -41,9 +43,9 @@ async def send_direct_message(messageid):
         caption_text = (
             f"📦 *{short_name}*\n"
             f"--- --- --- --- ---\n"
-            f"💰 *السعر:* '{product_price} ' \n"
+            f"💰 *السعر:* '{formatted_price_before} ' \n"
             f"📉 *الخصم:* '{discount_percentage}%' \n"
-            f"🔥 *السعر بعد الخصم:* '{current_price} ' \n"
+            f"🔥 *السعر بعد الخصم:* '{formatted_price_after} ' \n"
         )        
 
 
